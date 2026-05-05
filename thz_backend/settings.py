20523797ed72d34edef9a05f6a39c3c0095b230b
FlayyX <kyawmintuna02@gmail.com>
2026-05-05 17:27:47 +0630

    Fix allowed Host

--- thz_backend/settings.py
+++ thz_backend/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-lwy0l#x&&38ee*j2un!223axu9+nrmt=63i!xmgjv6=s0q_@0k
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['powertech.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
